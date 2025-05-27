3e8a605f19d966ae2bf69d4837c8675d1d963473
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2025-05-27 21:55:09 +0700

    fix: attack logic

--- internal/game/player.go
+++ internal/game/player.go
@@ -3,6 +3,7 @@ package game
 import (
 	"clash-royale/internal/config"
 	"clash-royale/internal/network"
+	"fmt"
 	"sync"
 )
 
@@ -16,13 +17,43 @@ type Player struct {
 }
 
 func (p *Player) Attack(o *Player, command *Command) network.Message {
-	troop := p.Troops[command.TroopIndex]
+	// CASE 0: troop out of index
+	if command.TroopIndex < 0 || command.TroopIndex > 2 {
+		return network.Message{Type: config.MsgError, Data: "🤖 invaid index"}
+	}
+	troop := &p.Troops[command.TroopIndex]
+
+	// CASE 1: Not enough mana
 	if p.Mana <= 0 || p.Mana < troop.Mana {
 		return network.Message{Type: config.MsgError, Data: "You don't have enough mana!"}
 	}
-	p.Mana -= troop.Mana
 
-	tower := o.Towers[command.TowerIndex]
+	// CASE 2: Troop destroyed
+	if troop.HP <= 0 {
+		return network.Message{Type: config.MsgError, Data: fmt.Sprintf("🤖 %s destroyed!", troop.Name)}
+	}
+
+	// CASE 3: Wrong target tower
+	switch command.TowerIndex {
+	case 0:
+		if !((o.Towers[0].HP > 0) && (o.Towers[0].HP <= o.User.Metadata.Towers[0].HP) && (o.Towers[1].HP == o.User.Metadata.Towers[1].HP) && (o.Towers[2].HP == o.User.Metadata.Towers[2].HP)) {
+			return network.Message{Type: config.MsgError, Data: "🏰 invaid index"}
+		}
+	case 1:
+		if !((o.Towers[0].HP <= 0) && (o.Towers[1].HP > 0) && (o.Towers[1].HP <= o.User.Metadata.Towers[1].HP) && (o.Towers[2].HP == o.User.Metadata.Towers[2].HP)) {
+			return network.Message{Type: config.MsgError, Data: "🏰 invaid index"}
+		}
+	case 2:
+		if !((o.Towers[0].HP <= 0) && (o.Towers[1].HP <= 0) && (o.Towers[2].HP > 0) && (o.Towers[2].HP <= o.User.Metadata.Towers[2].HP)) {
+			return network.Message{Type: config.MsgError, Data: "🏰 invaid index"}
+		}
+	default:
+		return network.Message{Type: config.MsgError, Data: "🏰 not found!"}
+	}
+
+	tower := &o.Towers[command.TowerIndex]
+
+	p.Mana -= troop.Mana
 
 	crit := tower.HasCrit()
 	// fmt.Println(crit)
@@ -33,15 +64,16 @@ func (p *Player) Attack(o *Player, command *Command) network.Message {
 	} else {
 		dmgToTroop = max(tower.ATK-troop.DEF, 0.0)
 	}
-	troop.HP -= dmgToTroop
+	troop.HP = max(troop.HP-dmgToTroop, 0.0)
 
 	dmgToTower := max(troop.ATK-tower.DEF, 0.0)
-	tower.HP -= dmgToTower
+	tower.HP = max(tower.HP-dmgToTower, 0.0)
+
 	return network.Message{Type: config.MsgAttackResult, Data: CombatResult{
 		Attacker:     p.User.Metadata.Username,
 		Defender:     o.User.Metadata.Username,
-		UsingTroop:   troop,
-		TargetTower:  tower,
+		UsingTroop:   *troop,
+		TargetTower:  *tower,
 		DamgeToTroop: dmgToTroop,
 		DamgeToTower: dmgToTower,
 	}}
